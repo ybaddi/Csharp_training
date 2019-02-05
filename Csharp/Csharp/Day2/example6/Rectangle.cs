28810860c091424c30f24a3ccf2b0438ae68ef0d
baddi <baddiyoussef85@gmail.com>
2019-02-05 20:26:13 +0100

    add all example in day 2

---
+++ Csharp/Csharp/Day2/example6/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Csharp.Day2.example6
+{
+    class Rectangle : Shape
+    {
+        public override double area()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Csharp/Csharp/Day2/example6/Shape.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Csharp.Day2.example6
+{
+
+
+    //NAme
+    // virtual GetInfo
+    // methode abstract so the class must be also abstract
+    abstract class Shape
+    {
+
+        public abstract double area();
+    }
+}
